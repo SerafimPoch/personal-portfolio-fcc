5ab0139ca556b416a7955409bed4ee28553db367
SerafimPoch <serpochapinsky@gmail.com>
2018-10-07 14:13:13 +0300

    Updates

---
+++ precache-manifest.52df89a65149acf5f39a3dd49f1b7982.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "72266ea6d24ec77e29ae",
+    "url": "/portfolio-page-fcc/static/js/runtime~main.72266ea6.js"
+  },
+  {
+    "revision": "8a45fc9c4861b670d04d",
+    "url": "/portfolio-page-fcc/static/js/main.8a45fc9c.chunk.js"
+  },
+  {
+    "revision": "b99f27b3cb84157e8901",
+    "url": "/portfolio-page-fcc/static/js/1.b99f27b3.chunk.js"
+  },
+  {
+    "revision": "925aa970d4a135265527ccc1ddbb33b8",
+    "url": "/portfolio-page-fcc/index.html"
+  }
+];
